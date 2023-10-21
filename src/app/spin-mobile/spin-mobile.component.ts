8cec3c41121b468227eaba3378b848c148cf8b47
azizlf <developer.mohamedazizlouifi@gmail.com>
2023-10-22 00:59:26 +0200

    new_version

--- src/app/spin-mobile/spin-mobile.component.ts
+++ src/app/spin-mobile/spin-mobile.component.ts
@@ -231,6 +231,28 @@ export class SpinMobileComponent implements OnInit {
 
   currentTime = 0
 
+  lastTikets:any = []
+
+  numbersTable:any = []
+
+  numbers:any = [1,13,25,2,14,26,3,15,27,4,16,28,5,17,29,6,18,30,7,19,31,8,20,32,9,21,33,10,22,34,11,23,35,12,24,36]
+
+  zeroOccur = 0
+
+  redOccur = 0
+  blackOccur = 0
+  greenOccur = 0
+
+  onto12Occur = 0
+  thrtto24Occur = 0
+  twntfto136ccur = 0
+
+  doensOccur = 0
+
+  hotNumbers:any = []
+
+  coldNumbers:any = []
+
 
   constructor(private rouletteService:RouletteService,private users:UsersService,private tiketService:TiketService) { }
 
@@ -307,8 +329,6 @@ export class SpinMobileComponent implements OnInit {
 
   chronoConfig(){
 
-    console.log(this.currentTime)
-
     this.timerChrono = ((((this.timeChrono-32) - this.currentTime)/(this.timeChrono-32))*100) +"%"
     
     if(this.currentTime >= (this.timeChrono-32) && this.currentTime < this.timeChrono && !this.isSpinning){
@@ -335,11 +355,235 @@ export class SpinMobileComponent implements OnInit {
 
   }
 
+  generateCode(length:any) {
+
+    const numbers = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPMLKJHGFDQSWXCVBN0123456789'
+
+    var result = ''
+
+    for (var i = 0 ;i < length; i++) {
+
+      const index = Math.floor(Math.random() * numbers.length)
+
+      result += numbers.charAt(index)
+    }
+
+      return result
+
+  }
+
+
+  checkNumberColor(n:any){
+    
+    if(n === 0){
+      return "green"
+    }
+    else if(n%2 === 0){
+      return "black"
+    }
+    else{
+      return "red"
+    }
+
+  }
+
+  manageHotCold(){
+
+    this.hotNumbers = []
+    this.coldNumbers = []
+
+    this.numbersTable.forEach((nbr:any)=>{
+
+      nbr.forEach((n:any)=>{
+
+        if(n.occur >= 2){
+          if(this.hotNumbers.length < 5){
+            this.hotNumbers.push({
+
+              number:n.number,
+              color:n.color,
+
+            })
+          }
+        }else{
+          if(this.coldNumbers.length < 5){
+            this.coldNumbers.push({
+
+              number:n.number,
+              color:n.color,
+
+            })
+          }
+        }
+
+      })
+
+    })
+
+
+  }
+
+  manageColor(n:any){
+
+    if(n === 0){
+      this.greenOccur++
+    }
+    else if(n%2 === 0){
+      this.blackOccur++
+    }
+    else{
+      this.redOccur++
+    }
+
+  }
+
+  manageDozens(n:any){
+
+    if(n >= 1 && n <= 12){
+      this.onto12Occur++
+      this.doensOccur++
+    }
+    else if(n >= 13 && n <= 24){
+      this.thrtto24Occur++
+      this.doensOccur++
+    }
+    else if(n >= 25 && n <= 36){
+      this.twntfto136ccur++
+      this.doensOccur++
+    }
+
+  }
+
+
+  historyMng(history:any){
+
+    this.zeroOccur = 0
+
+    this.redOccur = 0
+    this.blackOccur = 0
+    this.greenOccur = 0
+
+    this.onto12Occur = 0
+    this.thrtto24Occur = 0
+    this.twntfto136ccur = 0
+
+    this.doensOccur = 0
+
+
+    const list = history.slice(0,40)
+
+    this.lastTikets = []
+
+    for (var i = 0 ;i < 8; i++) {
+
+      this.lastTikets.push({
+
+        number:list[i],
+        color:this.checkNumberColor(list[i]),
+        code:"#"+this.generateCode(10)
+
+      })
+
+    }
+
+    this.generateSection()
+
+
+    list.forEach((n:any)=>{
+
+      this.manageDozens(n)
+
+      this.manageColor(n)
+
+      if(n === 0){
+
+        this.zeroOccur += 1
+
+      }
+
+      this.numbersTable.forEach((nbr:any)=>{
+        
+        nbr.forEach((nb:any)=>{
+
+
+          if(nb.number === n){
+            nb.occur += 1
+          }
+
+        })
+        
+
+      })
+
+    })
+
+    this.manageHotCold()
+
+
+    this.element = document.querySelector(".lignes-container")
+
+    this.element.innerHTML = ""
+
+    this.numbersTable.forEach((sc:any)=>{
+
+      var html = `<div class="ligne-nbrs">
+
+                    <div class="key ${sc[0].color}">${sc[0].number}</div>
+                    <div class="value">${sc[0].occur}</div>
+
+                    <div class="key ${sc[1].color}">${sc[1].number}</div>
+                    <div class="value">${sc[1].occur}</div>
+
+                    <div class="key ${sc[2].color}">${sc[2].number}</div>
+                    <div class="value">${sc[2].occur}</div>
+
+                  </div>`
+
+      
+      this.element.innerHTML += html
+
+    })
+
+    var html = `<div class="ligne-nbrs end">
+                  <div class="key green">0</div>
+                  <div class="value">${this.zeroOccur}</div>
+                </div>`
+    this.element.innerHTML += html
+  }
+
+
+  generateSection(){
+    this.numbersTable = []
+    var counter = 1
+    var section:any = []
+
+    for (var i = 0 ;i < this.numbers.length; i++) {
+
+      section.push({
+
+        number:this.numbers[i],
+        occur:0,
+        color:this.checkNumberColor(this.numbers[i])
+
+      })
+
+      if(counter < 3){
+        
+        counter++
+
+      }else{
+        this.numbersTable.push(section)
+        section = []
+        counter = 1
+      }
+
+    }
+  }
+
+
   ngOnInit(): void {
 
     this.users.findAdmin(localStorage.getItem("#FSDJIOSFDEZ")).subscribe((res:any)=>{
-
-      console.log(res.historique)
       
       this.initSpin(res.resultatRoulette)
       setTimeout(()=>{
@@ -364,6 +608,11 @@ export class SpinMobileComponent implements OnInit {
               setTimeout(()=>{
                 this.element = document.querySelector(".spin-section")
                 this.element.style.scale = "1"
+                this.users.findAdmin(localStorage.getItem("#FSDJIOSFDEZ")).subscribe((res:any)=>{
+
+                  this.historyMng(res.hist)
+
+                })
               },100)
               this.element = document.querySelector(".spin")
               this.element.style.transition = "transform 0s"
